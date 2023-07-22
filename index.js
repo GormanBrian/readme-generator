f34e48b54e3459f6ef8ade0cf62d6ab8ae58320c
Brian Gorman <briangorman99@gmail.com>
2023-07-22 00:47:18 -0400

    add open source and open data license data

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
 import path from "path";
-import { licenses, getLicense } from "./utils/licenses.js";
+import fs from "fs";
+import {
+  licenses,
+  getLicenseByName,
+  getSublicenseByName,
+  createLicenseBadge,
+} from "./utils/licenses.js";
+import generateMarkdown from "./utils/generateMarkdown.js";
+
+const loadJSON = (path) =>
+  JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
+let pkg = loadJSON("./package.json");
 
 /**
  * 1. Title
@@ -18,61 +29,67 @@ const questions = [
     type: "input",
     name: "title",
     message: "Enter the title of the project",
-    default: path.basename(path.resolve()),
+    default: pkg.name !== "" ? pkg.name : path.basename(path.resolve()),
   },
   {
     type: "input",
     name: "description",
-    message: "Enter a description of this project",
+    message: "Enter a description:",
   },
   {
     type: "input",
     name: "installation",
-    message: "What are the installation instructions?",
+    message: "Enter the installation instructions:",
     default: "npm init",
   },
   {
     type: "list",
     name: "license",
-    message: "What license is this project using?",
+    message: "Select the license:",
     choices: licenses.map((l) => l.name),
   },
   {
     type: "list",
     name: "specificLicense",
-    message: "What license is this project using?",
+    message: "Select the specific license:",
+    when: ({ license }) => "sublicenses" in getLicenseByName(license),
     choices: ({ license }) =>
-      getLicense(license).sublicenses.map((l) => l.name),
-    when: ({ license }) => {
-      let licenseObj = getLicense(license);
-      return licenseObj && "sublicenses" in licenseObj;
-    },
+      getLicenseByName(license).sublicenses.map((l) => l.name),
   },
   {
     type: "confirm",
     name: "canContribute",
-    message: "Are others allowed to contribute to this project?",
+    message: "Are contributions allowed?",
   },
   {
     type: "input",
     name: "contribute",
-    message: "What are the directions for contributing to this project",
+    message: "Enter the contribution instructions:",
     when: ({ canContribute }) => canContribute,
   },
   {
     type: "input",
     name: "tests",
-    message: "What are the testing instructions?",
+    message: "Enter the testing instructions:",
+    default: pkg.scripts.test,
   },
   {
     type: "input",
     name: "github",
-    message: "What is your GitHub username?",
+    message: "Enter your GitHub username:",
+    default: () => {
+      let url = pkg.url.toLowerCase();
+      if (url.includes("github.com")) {
+        if (url.slice(-1) === "/") url = url.slice(0, -1);
+        return url.slice(0, url.lastIndexOf("/"));
+      } else return undefined;
+    },
   },
   {
     type: "input",
     name: "email",
-    message: "What is your email address?",
+    message: "Enter your email address:",
+    default: pkg.email,
   },
 ];
 
@@ -81,10 +98,30 @@ function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions).then((answers) => {
-    console.log(answers);
-  });
+  inquirer
+    .prompt(questions)
+    .then(
+      ({
+        title,
+        description,
+        installation,
+        license,
+        sublicense,
+        canContribute,
+        tests,
+        github,
+        email,
+      }) => {
+        let licenseBadge = createLicenseBadge(
+          sublicense
+            ? getSublicenseByName(license, sublicense)
+            : getLicenseByName(license)
+        );
+      }
+    );
 }
+
+console.log(licenses.length);
 
 // Function call to initialize app
 init();
--- utils/licenses.js
+++ utils/licenses.js
@@ -1,22 +1,30 @@
+/**
+ * List of popular open source and open data licenses with badge data
+ * Source: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+ */
 export const licenses = [
   {
     name: "MIT",
-    link: "",
+    badge: "https://img.shields.io/badge/License-MIT-yellow.svg",
+    link: "https://opensource.org/licenses/MIT",
   },
   {
     name: "Apache 2.0",
-    link: "",
+    badge: "https://img.shields.io/badge/License-Apache_2.0-blue.svg",
+    link: "https://opensource.org/licenses/Apache-2.0",
   },
   {
     name: "BSD",
     sublicenses: [
       {
         name: "BSD 3-Clause",
-        link: "",
+        badge: "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg",
+        link: "https://opensource.org/licenses/BSD-3-Clause",
       },
       {
         name: "BSD 2-Clause",
-        link: "",
+        badge: "https://img.shields.io/badge/License-BSD_2--Clause-orange.svg",
+        link: "https://opensource.org/licenses/BSD-2-Clause",
       },
     ],
   },
@@ -25,10 +33,126 @@ export const licenses = [
     sublicenses: [
       {
         name: "CC0",
-        link: "",
+        slug: "CC0-1.0",
+        badge: "https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg",
+        link: "http://creativecommons.org/publicdomain/zero/1.0/",
+      },
+      {
+        name: "Attribution 4.0 International",
+        slug: "CC BY 4.0",
+        badge: "https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg",
+        link: "https://creativecommons.org/licenses/by/4.0/",
+      },
+      {
+        name: "Attribution-ShareAlike 4.0 International",
+        slug: "CC BY-SA 4.0",
+        badge:
+          "https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg",
+        link: "https://creativecommons.org/licenses/by-sa/4.0/",
+      },
+      {
+        name: "Attribution-NonCommercial 4.0 International",
+        slug: "CC BY-NC 4.0",
+        badge:
+          "https://img.shields.io/badge/License-CC_BY--NC_4.0-lightgrey.svg",
+        link: "https://creativecommons.org/licenses/by-nc/4.0/",
+      },
+      {
+        name: "Attribution-NoDerivates 4.0 International",
+        slug: "CC BY-ND 4.0",
+        badge:
+          "https://img.shields.io/badge/License-CC_BY--ND_4.0-lightgrey.svg",
+        link: "https://creativecommons.org/licenses/by-nd/4.0/",
+      },
+      {
+        name: "Attribution-NonCommmercial-ShareAlike 4.0 International",
+        slug: "CC BY-NC-SA 4.0",
+        badge:
+          "https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-lightgrey.svg",
+        link: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+      },
+      {
+        name: "Attribution-NonCommercial-NoDerivatives 4.0 International",
+        slug: "CC BY-NC-ND 4.0",
+        badge:
+          "https://img.shields.io/badge/License-CC_BY--NC--ND_4.0-lightgrey.svg",
+        link: "https://creativecommons.org/licenses/by-nc-nd/4.0/",
+      },
+    ],
+  },
+  {
+    name: "Mozilla Public License 2.0",
+    slug: "MPL 2.0",
+    badge: "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg",
+    link: "https://opensource.org/licenses/MPL-2.0",
+  },
+  {
+    name: "GNU",
+    sublicenses: [
+      {
+        name: "GNU GPL v3",
+        slug: "GPL v3",
+        badge: "https://img.shields.io/badge/License-GPLv3-blue.svg",
+        link: "https://www.gnu.org/licenses/gpl-3.0",
+      },
+      {
+        name: "GNU GPL v2",
+        slug: "GPL v2",
+        badge: "https://img.shields.io/badge/License-GPL_v2-blue.svg",
+        link: "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html",
+      },
+      {
+        name: "GNU AGPL v3",
+        slug: "AGPL v3",
+        badge: "https://img.shields.io/badge/License-AGPL_v3-blue.svg",
+        link: "https://www.gnu.org/licenses/agpl-3.0",
+      },
+      {
+        name: "GNU LGPL v3",
+        slug: "LGPL v3",
+        badge: "https://img.shields.io/badge/License-LGPL_v3-blue.svg",
+        link: "https://www.gnu.org/licenses/lgpl-3.0",
+      },
+      {
+        name: "GNU FDL v1.3",
+        slug: "FDL 1.3",
+        badge: "https://img.shields.io/badge/License-FDL_v1.3-blue.svg",
+        link: "https://www.gnu.org/licenses/fdl-1.3",
+      },
+    ],
+  },
+  {
+    name: "Open Data Commons",
+    sublicenses: [
+      {
+        name: "Attribution License (BY)",
+        slug: "Open Data Commons Attribution",
+        badge: "https://img.shields.io/badge/License-ODC_BY-brightgreen.svg",
+        link: "https://opendatacommons.org/licenses/by/",
+      },
+      {
+        name: "Open Database License (ODbL)",
+        slug: "ODbL",
+        badge: "https://img.shields.io/badge/License-ODbL-brightgreen.svg",
+        link: "https://opendatacommons.org/licenses/odbl/",
+      },
+      {
+        name: "Public Domain Dedication and License (PDDL)",
+        slug: "PDDL",
+        badge: "https://img.shields.io/badge/License-PDDL-brightgreen.svg",
+        link: "https://opendatacommons.org/licenses/pddl/",
       },
     ],
   },
 ];
 
-export const getLicense = (str) => licenses.find((l) => l.name === str);
+export const getLicenseByName = (licenseName) =>
+  licenses.find((l) => l.name === licenseName);
+
+export const getSublicenseByName = (licenseName, sublicenseName) =>
+  licenses
+    .find((l) => l.name === licenseName)
+    .sublicenses.find((s) => s.name === sublicenseName);
+
+export const createLicenseBadge = ({ badge, link, slug }) =>
+  `[![License${slug ? ": " + slug : ""}](${badge})](${link})`;
